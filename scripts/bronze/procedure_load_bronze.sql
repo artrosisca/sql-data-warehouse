6504c3c8f2b0ad5f37fa469570bc4cbc1212c389
arthu <arthu@THURN>
2026-04-15 01:13:20 -0300

    init dw change single-user changed

--- scripts/bronze/procedure_load_bronze.sql
+++ scripts/bronze/procedure_load_bronze.sql
@@ -28,7 +28,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_cust_info
 		PRINT '>> Inserting data in bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'D:\Repositories\sql-data-warehouse\datasets\source_crm\cust_info.csv'
+		FROM 'C:\sql-datawarehouse\datasets\source_crm\cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -43,7 +43,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_prd_info
 		PRINT '>> Inserting data in bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'D:\Repositories\sql-data-warehouse\datasets\source_crm\prd_info.csv'
+		FROM 'C:\sql-datawarehouse\datasets\source_crm\prd_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -58,7 +58,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_sales_details
 		PRINT '>> Inserting data in bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'D:\Repositories\sql-data-warehouse\datasets\source_crm\sales_details.csv'
+		FROM 'C:\sql-datawarehouse\datasets\source_crm\sales_details.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -77,7 +77,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_cust_az12
 		PRINT '>> Inserting data in bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'D:\Repositories\sql-data-warehouse\datasets\source_erp\CUST_AZ12.csv'
+		FROM 'C:\sql-datawarehouse\datasets\source_erp\CUST_AZ12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -92,7 +92,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_loc_a101
 		PRINT '>> Inserting data in bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'D:\Repositories\sql-data-warehouse\datasets\source_erp\LOC_A101.csv'
+		FROM 'C:\sql-datawarehouse\datasets\source_erp\LOC_A101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -107,7 +107,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2
 		PRINT '>> Inserting data in bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'D:\Repositories\sql-data-warehouse\datasets\source_erp\PX_CAT_G1V2.csv'
+		FROM 'C:\sql-datawarehouse\datasets\source_erp\PX_CAT_G1V2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
--- scripts/init_datawarehouse.sql
+++ scripts/init_datawarehouse.sql
@@ -1,29 +1,29 @@
 /*
-============================
-Create Database and Schemas
-============================
-Script Purpose:
-  Create a new datawarehouse and three schemas: 'bronze', 'silver', 'gold'.
-WARNING:
-  It will drop the ENTIRE DataWarehouse database if it exists.
+============================================================
+Create Database and Schemas (Fixed Version)
+============================================================
 */
+
 USE master;
 GO
 
--- Drop datawarehouse SE ele ja existe
+-- 1. Derruba conexões e exclui a base se ela já existir
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
-	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    -- Força o fechamento de conexões e coloca em single_user para deletar
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
 END;
 GO
 
--- Cria datawarehouse
+-- 2. Cria a base do zero
 CREATE DATABASE DataWarehouse;
+GO
 
 USE DataWarehouse;
 GO
 
--- Cria schemas
+-- 3. Cria os schemas para a arquitetura de medalhão
 CREATE SCHEMA bronze;
 GO
 CREATE SCHEMA silver;
