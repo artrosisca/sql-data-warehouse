1bdc062721ee5b6118de58a4cc636393cef6c1e3
artrosisca <arthur.rosisca@hotmail.com>
2026-04-17 17:30:36 -0300

    **Connection:** ThurN.DataWarehouse
     Add data quality checks and ETL for erp_loc_a101 tables
    
    Introduced scripts for data quality validation and transformation between bronze and silver layers for erp_loc_a101. Added checks for missing customer IDs, country code consistency, and standardized country names. Implemented an insert script to load and clean data from bronze to silver, ensuring consistent formatting and referential integrity.

---
+++ scripts/checks/data_quality_check_silver_erp_loc_a101.sql
@@ -0,0 +1,29 @@
+--//-- Check for missing cid 
+SELECT 
+REPLACE(cid, '-', '') AS cid
+FROM silver.erp_loc_a101
+WHERE REPLACE(cid, '-', '') 
+NOT IN (SELECT cst_key FROM silver.crm_cust_info)
+
+--//-- Check data consistency
+SELECT DISTINCT cntry
+FROM silver.erp_loc_a101
+ORDER BY cntry ASC
+
+--//--
+SELECT 
+REPLACE(cid, '-', '') AS cid,
+cntry,
+CASE --quick CASE nao suporta IN ou IS NULL
+    WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+    WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+    WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+    WHEN TRIM(cntry) = 'FR' THEN 'France'
+    ELSE TRIM(cntry)
+END AS cntry
+FROM silver.erp_loc_a101
+WHERE cntry = 'USA'
+
+--//--
+SELECT *
+FROM silver.erp_loc_a101
